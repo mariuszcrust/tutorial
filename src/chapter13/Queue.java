d817c376b4acda634d15c6beb86818eb6f1d1293
mariuszcrust <mariusz.danielewski@gmail.com>
2014-04-25 19:04:49 +0200

    Algorithm 1.3 FIFO queue

--- src/chapter13/Queue.java
+++ src/chapter13/Queue.java
@@ -2,12 +2,77 @@ package chapter13;
 
 import java.util.Iterator;
 
-public class Queue<T> implements Iterable<T> {
+public class Queue<Item> implements Iterable<Item> {
+
+    private Node first;
+    private Node last;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void enqueue(Item item) {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+
+        N++;
+
+    }
+
+    public Item dequeue() {
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty())
+            last = null;
+        N--;
+        return item;
+    }
 
     @Override
-    public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public void remove() {
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
--- src/chapter13/Stack.java
+++ src/chapter13/Stack.java
@@ -2,12 +2,63 @@ package chapter13;
 
 import java.util.Iterator;
 
-public class Stack<T> implements Iterable<T> {
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Stack<Item> implements Iterable<Item> {
+
+    Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+
+        return item;
+    }
 
     @Override
-    public Iterator<T> iterator() {
+    public Iterator<Item> iterator() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> s = new Stack<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                s.push(item);
+            } else if (item.equals("-")) {
+                StdOut.print(s.pop() + " ");
+            }
+        }
+
+        StdOut.println("(" + s.size() + " left on stack)");
+
     }
 
 }
